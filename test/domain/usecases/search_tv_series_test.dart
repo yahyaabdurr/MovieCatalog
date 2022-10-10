d7b325e30a7476b29dbd62274aff17b34b9a1b5d
Yahya Abdurrozaq <v.yahya.abdurrozaq@adira.co.id>
2022-10-10 13:49:19 +0700

    update usecase search test

---
+++ test/domain/usecases/search_tv_series_test.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/usecases/search_tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late SearchTvSeries usecase;
+  late MockTvRepository mockTvRepository;
+
+  setUp(() {
+    mockTvRepository = MockTvRepository();
+    usecase = SearchTvSeries(mockTvRepository);
+  });
+
+  final tMovies = <TvSeries>[];
+  final tQuery = 'hulk';
+
+  test('should get list of tv series from the repository', () async {
+    // arrange
+    when(mockTvRepository.searchTvSeries(tQuery))
+        .thenAnswer((_) async => Right(tMovies));
+    // act
+    final result = await usecase.execute(tQuery);
+    // assert
+    expect(result, Right(tMovies));
+  });
+}
